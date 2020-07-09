dbf1e973b9fbd181dc4e8b7a21f8429be8fe391f
ebrayce <gamiskah@gmail.com>
2020-07-09 10:26:56 +0000

    First Commit

---
+++ app/src/main/java/com/brayce/explicitintentsreciever/MainActivity.java
@@ -0,0 +1,31 @@
+package com.brayce.explicitintentsreciever;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtShowUrl = findViewById(R.id.text_uri_message);
+
+        Intent intent = getIntent();
+
+        Uri url = intent.getData();
+
+        if (url != null){
+
+            String showUrl = "URL: " + url.toString();
+            txtShowUrl.setText(showUrl);
+        }
+
+
+    }
+}
